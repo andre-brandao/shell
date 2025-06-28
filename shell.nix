0ae50423aa9d91bec1fb676e616d7c8be89846c4
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-06-27 23:34:09 -0300

    init

---
+++ shell.nix
@@ -0,0 +1,30 @@
+# Run with `nix-shell shell.nix`
+let
+  pkgs = import <nixpkgs> { };
+in
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    pkg-config
+    gobject-introspection
+    cargo
+    cargo-tauri # Optional, Only needed if Tauri doesn't work through the traditional way.
+    nodejs # Optional, this is for if you have a js frontend
+    bun
+  ];
+
+  buildInputs = with pkgs; [
+    at-spi2-atk
+    atkmm
+    cairo
+    gdk-pixbuf
+    glib
+    gtk3
+    harfbuzz
+    librsvg
+    libsoup_3
+    pango
+    webkitgtk_4_1
+    openssl
+  ];
+  # shellHook = "";
+}
