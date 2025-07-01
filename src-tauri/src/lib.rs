710317bbd84f335db279c3908a2a56005f373522
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2025-06-30 21:55:12 -0300

    refact

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,54 +1,18 @@
-mod hypr;
-mod os;
+mod commands;
 mod setup;
-
-use gtk::prelude::{ContainerExt, GtkWindowExt, WidgetExt};
-use gtk_layer_shell::{Edge, KeyboardMode, Layer, LayerShell};
-
-use hypr::commands::*;
-use hypr::events::HyprlandEvents;
-use os::apps::*;
-use os::stats::*;
-
-// use std::collections::HashMap;
-// use tauri::Manager;
-// use tauri::{AppHandle, Emitter};
-// use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut, ShortcutState};
-
-use tauri::{menu::MenuBuilder, Manager};
-
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-#[tauri::command]
-fn toggle_launcher(app_handle: tauri::AppHandle) {
-    let window = app_handle
-        .get_webview_window("launcher")
-        .expect("launcher window not found");
-
-    match window.is_visible() {
-        Ok(true) => window.hide().unwrap(),
-        Ok(false) => window.show().unwrap(),
-        Err(e) => println!("Error checking visibility: {}", e),
-    }
-}
 
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_os::init())
         .invoke_handler(tauri::generate_handler![
-            greet,
-            // Hyprland data commands
-            change_workspace,
-            get_battery_sys,
-            get_system_info,
-            get_disk_info,
-            get_apps,
-            get_running_apps,
-            toggle_launcher,
+            commands::change_workspace,
+            commands::toggle_launcher,
+            commands::get_battery_sys,
+            commands::get_system_info,
+            commands::get_disk_info,
+            commands::get_apps,
+            commands::get_running_apps,
+            // toggle_launcher,
         ])
         .setup(|app| {
             setup::bar::setup_bar(app.handle().clone());
@@ -58,74 +22,4 @@ pub fn run() {
         })
         .run(tauri::generate_context!())
         .unwrap()
-}
-
-fn create_launcher(app: tauri::AppHandle) {
-    // let webview_window = create_window(app, "launcher", "/launcher");
-    let webview_window = tauri::WebviewWindowBuilder::new(
-        &app,
-        "launcher",
-        tauri::WebviewUrl::App("/launcher".into()),
-    )
-    .inner_size(800.0, 600.0)
-    .always_on_top(true)
-    .decorations(false)
-    .center()
-    .transparent(true)
-    .maximized(false)
-    .build()
-    .unwrap();
-    // webview_window.set_decorations(false).unwrap();
-    // webview_window.set_always_on_top(true).unwrap();
-    // webview_window.set_min_size(size)
-
-    // // webview_window.gt
-    // let gtk_win = webview_window
-    //     .gtk_window()
-    //     .expect("Failed to get GTK window for launcher");
-    // gtk_win.set_keep_above(true);
-    // gtk_win.init_layer_shell();
-    // gtk_win.set_layer(gtk_layer_shell::Layer::Overlay);
-
-    // -----------------GTK WINDOW------------------------
-    // webview_window.hide().unwrap();
-
-    // let gtk_window =
-    //     gtk::ApplicationWindow::new(&webview_window.gtk_window().unwrap().application().unwrap());
-    // gtk_window.set_app_paintable(true);
-
-    // let vbox = webview_window.default_vbox().unwrap();
-    // webview_window.gtk_window().unwrap().remove(&vbox);
-    // gtk_window.add(&vbox);
-
-    // gtk_window.init_layer_shell();
-    // gtk_window.set_layer(gtk_layer_shell::Layer::Overlay);
-
-    // // gtk_window.set_monitor(&monitor);
-    // gtk_window.set_layer_shell_margin(Edge::Left, 40);
-    // gtk_window.set_layer_shell_margin(Edge::Right, 40);
-    // gtk_window.set_layer_shell_margin(Edge::Top, 20);
-
-    // // Center the window (this works with layer shell)
-    // gtk_window.set_anchor(Edge::Top, false);
-    // gtk_window.set_anchor(Edge::Bottom, false);
-    // gtk_window.set_anchor(Edge::Left, false);
-    // gtk_window.set_anchor(Edge::Right, false);
-
-    // // gtk_window.set_keyboard_mode(gtk_layer_shell::KeyboardMode::Exclusive); // to allow keyboard input
-    // gtk_window.set_keyboard_interactivity(true);
-    // gtk_window.set_height_request(640);
-    // gtk_window.set_width_request(640);
-    // gtk_window.set_position(gtk::WindowPosition::Center);
-    // gtk_window.show
-
-    // // set anchor top left and right
-    // // gtk_window.set_anchor(gtk_layer_shell::IsA::);
-    // // gtk_window.set_anchor(gtk_layer_shell::Edge::Left, true);
-    // // gtk_window.set_anchor(gtk_layer_shell::Edge::Right, true);
-
-    // gtk_window.show_all();
-    // gtk_window.hide();
-
-    // return (gtk_window, webview_window);
 }
